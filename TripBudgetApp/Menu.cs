7e0e4ab52529a19454ec2a33732b17181827b9c6
Tomasz Zukowski <tomaszzukowskibp@gmail.com>
2023-12-24 14:12:35 +0100

    Created main menu in Program.

--- TripBudgetApp/Menu.cs
+++ TripBudgetApp/Menu.cs
@@ -10,10 +10,10 @@ namespace TripBudgetApp
     public class Menu
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Name { get; set; }
 
         public override string ToString()
-        { return $"{Title}"; }
+        { return $"{Name}"; }
     }
 
     
--- TripBudgetApp/Program.cs
+++ TripBudgetApp/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            static List<Menu> Mainmenu()
+            {
+                return new List<Menu>()
+                {
+                    new Menu { Id = 1, Name = "Add budget position"}
+                    new Menu { Id = 2, Name = "Add person"}
+                    new Menu { Id = 3, Name = "Update position"}
+                    new Menu { Id = 4, Name = "Show all positions"}
+                    new Menu { Id = 5, Name = "Delete position"}
+                    new Menu { Id = 6, Name  = "Show details"}
+                    new Menu { Id = 7, Name = "Quit program"}
+                }
+                
+            }
         }
     }
 }
